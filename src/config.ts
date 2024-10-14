6e6ff594f4138e0495a2f376ca894c218867e1ee
yan <yanhaibiao1991@gmail.com>
2024-10-14 12:08:45 +0800

    update config

--- src/config.ts
+++ src/config.ts
@@ -2,16 +2,18 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-export let BASE_URL = process.env.NAVI_DEX_AGGREGATOR_API_BASE_URL || "";
-
-export const updateURL = (newBaseUrl: string): void => {
-    BASE_URL = newBaseUrl;
-};
-
 export const config = {
-    BASE_URL,
-    AGGREGATORCONTRACT: "0x88dfe5e893bc9fa984d121e4d0d5b2e873dc70ae430cf5b3228ae6cb199cb32b",
-    CETUSPACKAGEID: "0x70968826ad1b4ba895753f634b0aea68d0672908ca1075a2abdf0fc9e0b2fc6a",
-    CETUSCONFIGID: "0xdaa46292632c3c4d8f31f23ea0f9b36a28ff3677e9684980e4438403a67a3d8f",
-    TURBOSPACKAGEID: "0x1a3c42ded7b75cdf4ebc7c7b7da9d1e1db49f16fcdca934fac003f35f39ecad9"
+  BASE_URL: process.env.NAVI_DEX_AGGREGATOR_API_BASE_URL || "",
+  AGGREGATORCONTRACT:
+    "0x88dfe5e893bc9fa984d121e4d0d5b2e873dc70ae430cf5b3228ae6cb199cb32b",
+  CETUSPACKAGEID:
+    "0x70968826ad1b4ba895753f634b0aea68d0672908ca1075a2abdf0fc9e0b2fc6a",
+  CETUSCONFIGID:
+    "0xdaa46292632c3c4d8f31f23ea0f9b36a28ff3677e9684980e4438403a67a3d8f",
+  TURBOSPACKAGEID:
+    "0x1a3c42ded7b75cdf4ebc7c7b7da9d1e1db49f16fcdca934fac003f35f39ecad9",
 };
+
+export function updateConfig(newConfig: Partial<typeof config>) {
+  Object.assign(config, newConfig);
+}
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,101 +5,155 @@ import { makeCETUSPTB } from "./cetus";
 import { makeTurbosPTB } from "./turbos";
 import { config } from "../config";
 
-export async function getCoins(client: SuiClient, address: string, coinType: any = "0x2::sui::SUI") {
-    const coinAddress = coinType.address ? coinType.address : coinType;
+export async function getCoins(
+  client: SuiClient,
+  address: string,
+  coinType: any = "0x2::sui::SUI"
+) {
+  const coinAddress = coinType.address ? coinType.address : coinType;
 
-    const coinDetails = await client.getCoins({
-        owner: address,
-        coinType: coinAddress
-    });
-    return coinDetails;
+  const coinDetails = await client.getCoins({
+    owner: address,
+    coinType: coinAddress,
+  });
+  return coinDetails;
 }
 
-export async function getRoutePTBWithCoin(txb: Transaction, tokenA: string, tokenB: string, coinIn: TransactionResult, amountIn: number, minAmountOut: number, userAddress: string) {
+export async function getRoutePTBWithCoin(
+  txb: Transaction,
+  tokenA: string,
+  tokenB: string,
+  coinIn: TransactionResult,
+  amountIn: number,
+  minAmountOut: number,
+  userAddress: string
+) {
+  const data = await getRoutes(tokenA, tokenB, amountIn);
+  const allPaths = JSON.parse(JSON.stringify(data.data.routes));
+  const referral = 0;
+  if (!data.data.routes || data.data.routes.length === 0) {
+    throw new Error("No routes found in data");
+  }
 
-    const data = await getRoutes(tokenA, tokenB, amountIn);
-    const allPaths = JSON.parse(JSON.stringify(data.data.routes));
-    const referral = 0;
-    if (!data.data.routes || data.data.routes.length === 0) {
-        throw new Error("No routes found in data");
+  if (
+    Number(data.data.amount_in) !==
+    data.data.routes.reduce(
+      (sum: number, route: any) => sum + Number(route.amount_in),
+      0
+    )
+  ) {
+    throw new Error(
+      "Outer amount_in does not match the sum of route amount_in values"
+    );
+  }
+
+  const finalCoinB = txb.moveCall({
+    target: "0x2::coin::zero",
+    typeArguments: [tokenB],
+  });
+
+  for (let i = 0; i < allPaths.length; i++) {
+    const path = allPaths[i];
+    const pathCoinAmountIn = Math.floor(path.amount_in);
+    const pathCoinAmountOut = path.amount_out;
+    console.log(
+      `Path Index: `,
+      i,
+      `Amount In: `,
+      pathCoinAmountIn,
+      `Expected Amount Out: `,
+      pathCoinAmountOut
+    );
+    let pathTempCoin: any = txb.splitCoins(coinIn, [pathCoinAmountIn]);
+
+    for (let j = 0; j < path.path.length; j++) {
+      const route = path.path[j];
+
+      const poolId = route.id;
+      const provider = route.provider;
+      const tempTokenA = route.from;
+      const tempTokenB = route.target;
+      const a2b = route.a2b;
+      const typeArguments = route.info_for_ptb.typeArguments;
+
+      let amountInPTB;
+      let tuborsVersion;
+
+      if (provider === "turbos") {
+        tuborsVersion = route.info_for_ptb.contractVersionId;
+      }
+      console.log(
+        `Route Index: `,
+        i,
+        "-",
+        j,
+        `provider: `,
+        provider,
+        `from: `,
+        tempTokenA,
+        `to: `,
+        tempTokenB
+      );
+
+      amountInPTB = txb.moveCall({
+        target: "0x2::coin::value",
+        arguments: [pathTempCoin],
+        typeArguments: [tempTokenA],
+      });
+
+      if (provider === "cetus") {
+        let toSwapBalance = txb.moveCall({
+          target: "0x2::coin::into_balance",
+          arguments: [pathTempCoin],
+          typeArguments: [tempTokenA],
+        });
+        const { receiveCoin, leftCoin } = await makeCETUSPTB(
+          txb,
+          poolId,
+          true,
+          toSwapBalance,
+          amountInPTB,
+          a2b,
+          typeArguments
+        );
+
+        txb.transferObjects([leftCoin], userAddress);
+        pathTempCoin = receiveCoin;
+      } else if (provider === "turbos") {
+        pathTempCoin = txb.makeMoveVec({
+          elements: [pathTempCoin!],
+        });
+        const { turbosCoinB, turbosCoinA } = await makeTurbosPTB(
+          txb,
+          poolId,
+          true,
+          pathTempCoin,
+          amountInPTB,
+          a2b,
+          typeArguments,
+          userAddress,
+          tuborsVersion
+        );
+        txb.transferObjects([turbosCoinA], userAddress);
+        pathTempCoin = turbosCoinB;
+      }
     }
 
-    if (Number(data.data.amount_in) !== data.data.routes.reduce((sum: number, route: any) => sum + Number(route.amount_in), 0)) {
-        throw new Error("Outer amount_in does not match the sum of route amount_in values");
-    }
+    txb.mergeCoins(finalCoinB, [pathTempCoin]);
+  }
 
-    const finalCoinB = txb.moveCall({
-        target: '0x2::coin::zero',
-        typeArguments: [tokenB]
-    })
+  txb.transferObjects([coinIn], userAddress);
 
-    for (let i = 0; i < allPaths.length; i++) {
-        const path = allPaths[i];
-        const pathCoinAmountIn = Math.floor(path.amount_in);
-        const pathCoinAmountOut = path.amount_out;
-        console.log(`Path Index: `, i, `Amount In: `, pathCoinAmountIn, `Expected Amount Out: `, pathCoinAmountOut)
-        let pathTempCoin: any = txb.splitCoins(coinIn, [pathCoinAmountIn]);
+  txb.moveCall({
+    target: `${config.AGGREGATORCONTRACT}::slippage::check_slippage_v2`,
+    arguments: [
+      finalCoinB, // output coin object
+      txb.pure.u64(minAmountOut), // min amount out
+      txb.pure.u64(amountIn), // amount in
+      txb.pure.u64(referral), // refferal id
+    ],
+    typeArguments: [tokenA, tokenB],
+  });
 
-        for (let j = 0; j < path.path.length; j++) {
-            const route = path.path[j];
-
-            const poolId = route.id;
-            const provider = route.provider;
-            const tempTokenA = route.from;
-            const tempTokenB = route.target;
-            const a2b = route.a2b;
-            const typeArguments = route.info_for_ptb.typeArguments;
-
-            let amountInPTB;
-            let tuborsVersion;
-
-            if (provider === 'turbos') {
-                tuborsVersion = route.info_for_ptb.contractVersionId;
-            }
-            console.log(`Route Index: `, i, '-', j, `provider: `, provider, `from: `, tempTokenA, `to: `, tempTokenB)
-
-            amountInPTB = txb.moveCall({
-                target: '0x2::coin::value',
-                arguments: [pathTempCoin],
-                typeArguments: [tempTokenA]
-            });
-
-            if (provider === 'cetus') {
-                let toSwapBalance = txb.moveCall({
-                    target: '0x2::coin::into_balance',
-                    arguments: [pathTempCoin],
-                    typeArguments: [tempTokenA],
-                });
-                const { receiveCoin, leftCoin } = await makeCETUSPTB(txb, poolId, true, toSwapBalance, amountInPTB, a2b, typeArguments);
-
-                txb.transferObjects([leftCoin], userAddress);
-                pathTempCoin = receiveCoin;
-
-            } else if (provider === 'turbos') {
-                pathTempCoin = txb.makeMoveVec({
-                    elements: [pathTempCoin!],
-                });
-                const { turbosCoinB, turbosCoinA } = await makeTurbosPTB(txb, poolId, true, pathTempCoin, amountInPTB, a2b, typeArguments, userAddress, tuborsVersion);
-                txb.transferObjects([turbosCoinA], userAddress);
-                pathTempCoin = turbosCoinB;
-            }
-        }
-
-        txb.mergeCoins(finalCoinB, [pathTempCoin]);
-    }
-
-    txb.transferObjects([coinIn], userAddress);
-
-    txb.moveCall({
-        target: `${config.NAVIAGGREGATORCONTRACT}::slippage::check_slippage_v2`,
-        arguments: [
-            finalCoinB, // output coin object
-            txb.pure.u64(minAmountOut), // min amount out
-            txb.pure.u64(amountIn), // amount in
-            txb.pure.u64(referral) // refferal id
-        ],
-        typeArguments: [tokenA, tokenB],
-    })
-
-    return finalCoinB;
+  return finalCoinB;
 }
